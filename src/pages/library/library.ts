4d82d6e25096f5b72041965203b8cf88da71efc5
Gustavo Ramírez <gustavo81988@gmail.com>
2018-07-18 16:41:52 -0500

    Passing the Quotes Data between Pages

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Quote } from '../../data/quote.interface';
+import { QuotesPage } from '../quotes/quotes';
 import quotes from '../../data/quotes';
+
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html',
 })
 export class LibraryPage implements OnInit{
-  quoteCollection: {category: string, quotes: Quote[], icon:string}[];
+  quoteCollection: {
+    category: string, quotes: Quote[], icon:string
+  }[];
   
+  quotesPage = QuotesPage;
+
   ngOnInit(){
     this.quoteCollection = quotes;
   }
