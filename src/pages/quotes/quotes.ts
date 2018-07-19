674c695c4fb321d3121f623860a144d612c1de04
Gustavo Ramírez <gustavo81988@gmail.com>
2018-07-18 19:41:36 -0500

    Adding Alerts to the Application

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { NavParams, AlertController } from 'ionic-angular';
 import { Quote } from '../../data/quote.interface';
 
 @Component({
@@ -12,9 +12,37 @@ export class QuotesPage implements OnInit{
     category: string, quotes: Quote[], icon:string
   }
 
-  constructor(private navParams: NavParams){}
-  
+  constructor(
+    private navParams: NavParams,
+    private alertCtrl: AlertController,
+  ){}
+
   ngOnInit(){
     this.quoteGroup = this.navParams.data;
+  }
+
+  onAddToFavorite(selectedQuote: Quote){
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'No, I changed my mind',
+          role: 'cancel',
+          handler: ()=> {
+            console.log('Cancelled!')
+          }
+        },
+        {
+          text: 'Yes, go ahead',
+          handler: ()=> {
+            console.log('Ok')
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 }
