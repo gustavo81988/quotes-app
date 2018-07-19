836eb54b6eeebccd9484364a9935a4de6eb16b10
Gustavo Ramírez <gustavo81988@gmail.com>
2018-07-18 20:48:33 -0500

    Diving Deeper into List Items

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,25 +1,17 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { QuotesService } from '../../services/quotes';
+import { Quote } from '../../data/quote.interface';
 
-/**
- * Generated class for the FavoritesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-favorites',
   templateUrl: 'favorites.html',
 })
 export class FavoritesPage {
+  quotes: Quote[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private quotesService: QuotesService){}
+
+  ionViewWillEnter(){
+    this.quotes = this.quotesService.getFavoriteQuotes()
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FavoritesPage');
-  }
-
 }
